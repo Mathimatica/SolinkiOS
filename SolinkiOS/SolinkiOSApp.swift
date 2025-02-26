f9b8b6975c016485301560e27047605d431f468d
JoshPhillips <joshphillips.developer@gmail.com>
2025-02-26 14:58:25 -0500

    Initial Commit

---
+++ SolinkiOS/SolinkiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolinkiOSApp.swift
+//  SolinkiOS
+//
+//  Created by Josh Phillips on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct SolinkiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SolinkiOSTests/SolinkiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SolinkiOSTests.swift
+//  SolinkiOSTests
+//
+//  Created by Josh Phillips on 2025-02-26.
+//
+
+import Testing
+@testable import SolinkiOS
+
+struct SolinkiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
